246db27e325c5db9630db19d06bcd19075385a41
Norzhan <kaimetzing@aol.com>
2021-08-03 19:10:40 +0200

    Update startupChess.java

--- Chess/src/Startup/startupChess.java
+++ Chess/src/Startup/startupChess.java
@@ -11,6 +11,7 @@ public class startupChess
 
         Fen startposition = new Fen(
                 "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+        
 
         Position currentPosition = new Position(startposition);
 
