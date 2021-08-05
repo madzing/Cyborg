9769523b0d838fce6fe7c911a2d13d6f69024556
Nikalb <nikolai.albers@yahoo.de>
2021-08-05 12:32:14 +0200

    Test Cases hinzugefügt

--- Chess/src/Material/PositionTest.java
+++ Chess/src/Material/PositionTest.java
@@ -31,39 +31,76 @@ public class PositionTest {
 	@Test
 	public void testGetWhiteFiguren() {
 		List<Piece> white = new ArrayList<Piece>();
-		white.add(new Pawn((byte)48));
-		white.add(new Pawn((byte)49));
-		white.add(new Pawn((byte)50));
-		white.add(new Pawn((byte)51));
-		white.add(new Pawn((byte)52));
-		white.add(new Pawn((byte)53));
-		white.add(new Pawn((byte)54));
-		white.add(new Pawn((byte)55));
-		white.add(new Rook((byte)56));
-		white.add(new Knight((byte)57));
-		white.add(new Bishop((byte)58));
-		white.add(new Queen((byte)59));
-		white.add(new King((byte)60));
-		white.add(new Bishop((byte)61));
-		white.add(new Knight((byte)62));
-		white.add(new Rook((byte)63));
+		white.add(new Pawn((byte)48));		//0
+		white.add(new Pawn((byte)49));		//1
+		white.add(new Pawn((byte)50)); 		//2
+		white.add(new Pawn((byte)51)); 		//3
+		white.add(new Pawn((byte)52)); 		//4
+		white.add(new Pawn((byte)53)); 		//5
+		white.add(new Pawn((byte)54)); 		//6
+		white.add(new Pawn((byte)55)); 		//7
+		white.add(new Rook((byte)56)); 		//8
+		white.add(new Knight((byte)57));	//9
+		white.add(new Bishop((byte)58));	//10
+		white.add(new Queen((byte)59)); 	//11
+		white.add(new King((byte)60)); 		//12
+		white.add(new Bishop((byte)61));	//13
+		white.add(new Knight((byte)62)); 	//14
+		white.add(new Rook((byte)63)); 		//15
 
 		assertEquals(white.get(0).getClass(),_startPosition.getWhiteFiguren().get(0).getClass());
+		assertEquals(white.get(1).getClass(),_startPosition.getWhiteFiguren().get(1).getClass());
+		assertEquals(white.get(2).getClass(),_startPosition.getWhiteFiguren().get(2).getClass());
+		assertEquals(white.get(3).getClass(),_startPosition.getWhiteFiguren().get(3).getClass());
+		assertEquals(white.get(4).getClass(),_startPosition.getWhiteFiguren().get(4).getClass());
+		assertEquals(white.get(5).getClass(),_startPosition.getWhiteFiguren().get(5).getClass());
+		assertEquals(white.get(6).getClass(),_startPosition.getWhiteFiguren().get(6).getClass());
+		assertEquals(white.get(7).getClass(),_startPosition.getWhiteFiguren().get(7).getClass());
+		assertEquals(white.get(8).getClass(),_startPosition.getWhiteFiguren().get(8).getClass());
+		assertEquals(white.get(9).getClass(),_startPosition.getWhiteFiguren().get(9).getClass());
+		assertEquals(white.get(10).getClass(),_startPosition.getWhiteFiguren().get(10).getClass());
+		assertEquals(white.get(11).getClass(),_startPosition.getWhiteFiguren().get(11).getClass());
+		assertEquals(white.get(12).getClass(),_startPosition.getWhiteFiguren().get(12).getClass());
+		assertEquals(white.get(13).getClass(),_startPosition.getWhiteFiguren().get(13).getClass());
+		assertEquals(white.get(14).getClass(),_startPosition.getWhiteFiguren().get(14).getClass());
+		assertEquals(white.get(15).getClass(),_startPosition.getWhiteFiguren().get(15).getClass());
 	}
 
 	@Test
 	public void testGetBlackFiguren() {
 		List<Piece> black = new ArrayList<Piece>();
-		black.add(new Rook((byte)0));
-		black.add(new Knight((byte)1));
-		black.add(new Bishop((byte)2));
-		black.add(new Queen((byte)3));
-		black.add(new King((byte)4));
-		black.add(new Bishop((byte)5));
-		black.add(new Knight((byte)6));
-		black.add(new Rook((byte)7));
-		black.add(new Pawn((byte)8));
+		black.add(new Rook((byte)0));		//0
+		black.add(new Knight((byte)1));		//1
+		black.add(new Bishop((byte)2));		//2
+		black.add(new Queen((byte)3));		//3
+		black.add(new King((byte)4));		//4
+		black.add(new Bishop((byte)5));		//5		
+		black.add(new Knight((byte)6));		//6
+		black.add(new Rook((byte)7));		//7
+		black.add(new Pawn((byte)8));		//8
+		black.add(new Pawn((byte)9));		//9
+		black.add(new Pawn((byte)10));		//10
+		black.add(new Pawn((byte)11));		//11
+		black.add(new Pawn((byte)12));		//12
+		black.add(new Pawn((byte)13));		//13
+		black.add(new Pawn((byte)14));		//14
+		black.add(new Pawn((byte)15));		//15
 		assertEquals(black.get(0).getClass(),_startPosition.getBlackFiguren().get(0).getClass());
+		assertEquals(black.get(1).getClass(),_startPosition.getBlackFiguren().get(1).getClass());
+		assertEquals(black.get(2).getClass(),_startPosition.getBlackFiguren().get(2).getClass());
+		assertEquals(black.get(3).getClass(),_startPosition.getBlackFiguren().get(3).getClass());
+		assertEquals(black.get(4).getClass(),_startPosition.getBlackFiguren().get(4).getClass());
+		assertEquals(black.get(5).getClass(),_startPosition.getBlackFiguren().get(5).getClass());
+		assertEquals(black.get(6).getClass(),_startPosition.getBlackFiguren().get(6).getClass());
+		assertEquals(black.get(7).getClass(),_startPosition.getBlackFiguren().get(7).getClass());
+		assertEquals(black.get(8).getClass(),_startPosition.getBlackFiguren().get(8).getClass());
+		assertEquals(black.get(9).getClass(),_startPosition.getBlackFiguren().get(9).getClass());
+		assertEquals(black.get(10).getClass(),_startPosition.getBlackFiguren().get(10).getClass());
+		assertEquals(black.get(11).getClass(),_startPosition.getBlackFiguren().get(11).getClass());
+		assertEquals(black.get(12).getClass(),_startPosition.getBlackFiguren().get(12).getClass());
+		assertEquals(black.get(13).getClass(),_startPosition.getBlackFiguren().get(13).getClass());
+		assertEquals(black.get(14).getClass(),_startPosition.getBlackFiguren().get(14).getClass());
+		assertEquals(black.get(15).getClass(),_startPosition.getBlackFiguren().get(15).getClass());
 	}
 
 
