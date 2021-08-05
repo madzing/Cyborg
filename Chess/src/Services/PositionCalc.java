62d6ecd2acc7dfa251af136aeb3873dedf014751
Jan <jan.metzing@gmx.net>
2021-08-05 21:58:34 +0200

    PositionCalc angefangen

--- Chess/src/Services/PositionCalc.java
+++ Chess/src/Services/PositionCalc.java
@@ -4,6 +4,13 @@ import Material.Position;
 import Material.Piece;
 import Material.Pawn;
 import Material.Knight;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import Fachwerte.Coordinate;
 import Material.Bishop;
 import Material.Rook;
 import Material.Queen;
@@ -12,13 +19,72 @@ import Material.King;
 
 
 
-public  class PositionCalc {
+public  class PositionCalc 
+{
+
+	private static List<Piece> _figurenAmZug;
+	private static List<Piece> _figurenDesGegners;
+	private static List<Position> _folgePositionen = new ArrayList<Position>();
+	private static Map<String,Piece> _pinnedPieces = new HashMap<String,Piece>();
 	
-	public static void getlegalPositions(Position currentPosition)
+	
+	public static List<Position> getLegalPositions(Position currentPosition)
+	{
+		if(currentPosition.getZugrecht())
+		{
+			_figurenAmZug = currentPosition.getWhiteFiguren();
+			_figurenDesGegners = currentPosition.getBlackFiguren();			
+		}
+		else
+		{
+			_figurenAmZug = currentPosition.getBlackFiguren();
+			_figurenDesGegners = currentPosition.getWhiteFiguren();	
+		}
+		pinnedPieces();
+		insertLegalPawnMoves();
+		insertLegalKnightMoves();
+		insertLegalBishopMoves();
+		insertLegalRookMoves();
+		insertLegalQueenMoves();
+		insertLegalKingMoves();
+
+		return _folgePositionen;
+	}
+	
+	private static void insertLegalPawnMoves()
+	{
+			
+	}
+	
+	private static void insertLegalKnightMoves()
 	{
 		
 	}
 	
-
-
+	private static void insertLegalBishopMoves()
+	{
+		
+	}
+	
+	private static void insertLegalRookMoves()
+	{
+		
+	}
+	
+	private static void insertLegalQueenMoves()
+	{
+		
+	}
+	
+	private static void insertLegalKingMoves()
+	{
+		
+	}
+	
+	// Finde heraus, ob der König im Schach steht, wenn die Figur gelöscht ist. Sonderregel für enPassant ist nötig.
+	// Wenn dies für eine Figur zutrifft muss nach desses Movements geschaut werden, ob der König geschlagen werden kann.
+	private static void pinnedPieces()
+	{
+		
+	}
 }
