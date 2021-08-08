c149d37f8e89ba06e654a589144542d28d6b8e29
Jan <jan.metzing@gmx.net>
2021-08-08 17:13:34 +0200

    bla

--- Chess/src/Services/PositionCalcTest.java
+++ Chess/src/Services/PositionCalcTest.java
@@ -13,18 +13,18 @@ import Material.Position;
 public class PositionCalcTest {
 	Fen _startFen= Fen.select("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
 	Position _startPosition = new Position(_startFen);
-	
+	PositionCalc calc = new PositionCalc();
 	
 	@Test
 	public void getLegalPositions() {
-		List<Position> nodes = PositionCalc.getLegalPositions(_startPosition);
+		List<Position> nodes = calc.getLegalPositions(_startPosition);
 		//depth 1
 		assertEquals(20,nodes.size());	
 		int depth = 3;
 		for (int i = 1; i < depth; i++) {
 		List<Position> latestNodes = new ArrayList();
 		for(Position pos : nodes) {
-			latestNodes.addAll(PositionCalc.getLegalPositions(pos));
+			latestNodes.addAll(calc.getLegalPositions(pos));
 		}
 		nodes = latestNodes;
 		}
