39993c01b4809ab59a60808bad24ed0af205f2bb
Norzhan <kaimetzing@aol.com>
2021-08-08 15:20:29 +0200

    bla

--- Chess/src/Services/PositionCalcTest.java
+++ Chess/src/Services/PositionCalcTest.java
@@ -14,31 +14,20 @@ public class PositionCalcTest {
 	Fen _startFen= Fen.select("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
 	Position _startPosition = new Position(_startFen);
 	
+	
 	@Test
 	public void getLegalPositions() {
-		List<Position> firstPositions = PositionCalc.getLegalPositions(_startPosition);
-		assertEquals(20,firstPositions.size());
-		_startPosition.makeMove((byte)55,(byte) 39);
-		firstPositions = PositionCalc.getLegalPositions(_startPosition);
-		assertEquals(20,firstPositions.size());
-		_startPosition.makeMove((byte)8,(byte) 16);
-		firstPositions = PositionCalc.getLegalPositions(_startPosition);
-		assertEquals(21,firstPositions.size());
-		
-		//////////
-		
-		_startPosition = new Position(_startFen);
-	}
-	@Test
-	public void getLegalPositions2() {
 		List<Position> nodes = PositionCalc.getLegalPositions(_startPosition);
 		//depth 1
 		assertEquals(20,nodes.size());	
-		
+		int depth = 3;
+		for (int i = 1; i < depth; i++) {
 		List<Position> latestNodes = new ArrayList();
 		for(Position pos : nodes) {
 			latestNodes.addAll(PositionCalc.getLegalPositions(pos));
 		}
-		assertEquals(400,latestNodes.size());
+		nodes = latestNodes;
+		}
+		assertEquals(8902,nodes.size());
 }
 }
