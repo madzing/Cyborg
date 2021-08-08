688e83fd0130aac7d019fb5ff1ae546f51b8926a
Norzhan <kaimetzing@aol.com>
2021-08-08 17:11:21 +0200

    z

--- Chess/src/Material/Position.java
+++ Chess/src/Material/Position.java
@@ -704,4 +704,15 @@ public class Position
     {
         return _zuegeGesamt;
     }
+    @Override
+    public boolean equals(Object pos) {
+    	return (getFen().equals((((Position) pos).getFen())));
+    }
+//    @Override
+//    public int hashCode() {
+//    	return toInt(this.getFen());
+//    }
+//    public static int toInt(String fen) {
+//    	return Integer.parseInt(fen);
+//    }
 }
--- Chess/src/Services/PositionCalcTest.java
+++ Chess/src/Services/PositionCalcTest.java
@@ -3,7 +3,12 @@ package Services;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import org.junit.Test;
 
@@ -11,23 +16,35 @@ import Fachwerte.Fen;
 import Material.Position;
 
 public class PositionCalcTest {
-	Fen _startFen= Fen.select("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+	Fen _startFen = Fen.select("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
 	Position _startPosition = new Position(_startFen);
-	
-	
+	Position _startPosition2 = new Position(_startFen);
+
 	@Test
 	public void getLegalPositions() {
 		List<Position> nodes = PositionCalc.getLegalPositions(_startPosition);
-		//depth 1
-		assertEquals(20,nodes.size());	
+		// depth 1
+		assertEquals(20, nodes.size());
 		int depth = 3;
 		for (int i = 1; i < depth; i++) {
-		List<Position> latestNodes = new ArrayList();
-		for(Position pos : nodes) {
-			latestNodes.addAll(PositionCalc.getLegalPositions(pos));
+			List<Position> latestNodes = new ArrayList<>();
+			for (Position pos : nodes) {
+				latestNodes.addAll(PositionCalc.getLegalPositions(pos));
+			}
+			nodes = latestNodes;
 		}
-		nodes = latestNodes;
+		
+		assertEquals(8902, removeDuplicates(nodes).size());
+	}
+
+
+	public static Map<String, Position> removeDuplicates(List<Position> list) {
+		Map<String, Position> bla = new HashMap<String, Position>();
+		for (Position pos : list) {
+			bla.put(pos.getFen(), pos);
 		}
-		assertEquals(8902,nodes.size());
-}
+		return bla;
+
+	}
+
 }
