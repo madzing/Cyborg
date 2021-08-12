e7d2aa70c0d58497c89be8bb78425189639bc5f6
Jan <jan.metzing@gmx.net>
2021-08-12 23:28:34 +0200

    CodeBase steht

--- Chess/src/Services/Eval.java
+++ Chess/src/Services/Eval.java
@@ -2,27 +2,139 @@ package Services;
 
 import java.util.ArrayList;
 
+import Material.Bishop;
 import Material.King;
+import Material.Knight;
+import Material.Pawn;
 import Material.Piece;
 import Material.Position;
+import Material.Queen;
+import Material.Rook;
 
 public class Eval {
+	Double[] _pawnFelder;
+	Double[] _knightFelder;
+	Double[] _bishopFelder;
+	Double[] _rookFelder;
+	Double[] _queenFelder;
+	Double[] _kingFelder;
+	
 	public Eval() {
+		_pawnFelder = new Double[]{1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,
+								  1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,
+								  1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,
+								  1.0,1.0,1.0,1.7,1.7,1.0,1.0,1.0,
+								  1.0,1.0,1.0,1.7,1.7,1.0,1.0,1.0,
+								  1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,
+								  1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,
+								  1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,
+																};
+		_knightFelder = new Double[] {0.7,0.8,0.8,0.8,0.8,0.8,0.8,0.7,
+									 0.8,0.9,0.9,0.9,0.9,0.9,0.9,0.8,
+				  					 0.8,0.9,1.0,1.0,1.0,1.0,0.9,0.8,
+				  					 0.8,0.9,1.0,1.1,1.1,1.0,0.9,0.8,
+				  					 0.8,0.9,1.0,1.1,1.1,1.0,0.9,0.8,
+				  					 0.8,0.9,1.0,1.0,1.0,1.0,0.9,0.8,
+				  					 0.8,0.9,0.9,0.9,0.9,0.9,0.9,0.8,
+				  					 0.7,0.8,0.8,0.8,0.8,0.8,0.8,0.7,
+																	};
+		_bishopFelder = new Double[]{1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,
+				  					 1.0,1.1,1.0,1.0,1.0,1.0,1.1,1.0,
+				  					 1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,
+				  					 1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,
+				  					 1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,
+				  					 1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,
+				  					 1.0,1.1,1.0,1.0,1.0,1.0,1.1,1.0,
+				  					 1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,
+																	};
+		_rookFelder = new Double[]{1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,
+								   1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,
+								   1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,
+								   1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,
+								   1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,
+								   1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,
+								   1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,
+								   1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,
+																  };
+		_queenFelder = new Double[]{1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,
+				   					1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,
+				   					1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,
+				   					1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,
+				   					1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,
+				   					1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,
+				   					1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,
+				   					1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,
+												  				  };
+		_kingFelder = new Double[]{1.0,1.2,1.1,1.0,1.0,1.0,1.2,1.2,
+				   				   1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,
+				   				   1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,
+				   				   1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,
+				   				   1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,
+				   				   1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,
+				   				   1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,
+				   				   1.0,1.2,1.1,1.0,1.0,1.0,1.2,1.2,
+												  				};
 
 	}
 
-	public short getEval(Position position) {
+	public double getEval(Position position) {
 
-			short whitePoints = 0;
-			short blackPoints = 0;
+			double whitePoints = 0;
+			double blackPoints = 0;
 			for (Piece figur : position.getWhiteFiguren()) {
-				whitePoints = (short) (whitePoints + figur.getValue());
+				if(figur instanceof Pawn)
+				{
+					whitePoints = (double) (whitePoints + (figur.getValue()* _pawnFelder[figur.getCoordinate()]));
+				}
+				else if(figur instanceof Knight)
+				{
+					whitePoints = (double) (whitePoints + (figur.getValue()*_knightFelder[figur.getCoordinate()]));
+				}
+				else if(figur instanceof Bishop)
+				{
+					whitePoints = (double) (whitePoints + (figur.getValue()*_bishopFelder[figur.getCoordinate()]));
+				}
+				else if(figur instanceof Rook)
+				{
+					whitePoints = (double) (whitePoints + (figur.getValue()*_rookFelder[figur.getCoordinate()]));
+				}
+				else if(figur instanceof Queen)
+				{
+					whitePoints = (double) (whitePoints + (figur.getValue()*_queenFelder[figur.getCoordinate()]));
+				}
+				else 
+				{
+					whitePoints = (double) (whitePoints + (figur.getValue()*_kingFelder[figur.getCoordinate()]));
+				}
 			}
 			for (Piece figur : position.getBlackFiguren()) {
-				blackPoints = (short) (blackPoints + figur.getValue());
+				if (figur instanceof Pawn)
+				{
+					blackPoints = (double) (blackPoints + (figur.getValue()*_pawnFelder[figur.getCoordinate()]));
+				}
+				else if (figur instanceof Knight)
+				{
+					blackPoints = (double) (blackPoints + (figur.getValue()*_knightFelder[figur.getCoordinate()]));
+				}
+				else if (figur instanceof Bishop)
+				{
+					blackPoints = (double) (blackPoints + (figur.getValue()*_bishopFelder[figur.getCoordinate()]));
+				}
+				else if (figur instanceof Rook)
+				{
+					blackPoints = (double) (blackPoints + (figur.getValue()*_rookFelder[figur.getCoordinate()]));
+				}
+				else if (figur instanceof Queen)
+				{
+					blackPoints = (double) (blackPoints + (figur.getValue()*_queenFelder[figur.getCoordinate()]));
+				}
+				else 
+				{
+					blackPoints = (double) (blackPoints + (figur.getValue()+_kingFelder[figur.getCoordinate()]));
+				}
 			}
 
-			return (short) (whitePoints - blackPoints);
+			return (double) (whitePoints - blackPoints);
 		}
 
 }
--- Chess/src/Werkzeuge/RekursiverCyborg.java
+++ Chess/src/Werkzeuge/RekursiverCyborg.java
@@ -24,7 +24,7 @@ public class RekursiverCyborg
     public Position getBestFollowingPosition(Position position)
     {
 		Position bestPosition = null;
-		short bestEval = 0;
+		double bestEval = 0;
 		ArrayList<Position> legalPositions = new PositionCalc(position).getLegalFollowingPositions();
 		for(Position pos : legalPositions)
 		{
@@ -49,9 +49,9 @@ public class RekursiverCyborg
     	return bestPosition;
     }
 
-    public Short getEvaluation(Position position, int tiefe)
+    public Double getEvaluation(Position position, int tiefe)
     {
-    	ArrayList<Short> list = new ArrayList<Short>();
+    	ArrayList<Double> list = new ArrayList<Double>();
     	if(tiefe == 0)
     	{
     		list.add(_eval.getEval(position));
@@ -71,10 +71,10 @@ public class RekursiverCyborg
     					{
     						if(((King) piece).isInCheck(position))
     						{
-    							list.add((short)-127);
+    							list.add((double) -127);
     						}
     						else {
-    							list.add((short)0);
+    							list.add((double)0);
     						}
     					}
     				}
@@ -87,11 +87,11 @@ public class RekursiverCyborg
     					{
     						if(((King) piece).isInCheck(position))
     						{
-    							list.add((short)127);
+    							list.add((double)127);
     						}
     						else
     						{
-    							list.add((short)0);
+    							list.add((double)0);
     						}
     					}
     				}	
@@ -116,11 +116,11 @@ public class RekursiverCyborg
     	}
     }
     
-    public short bestValueforWhite(ArrayList<Short> list)
+    public double bestValueforWhite(ArrayList<Double> list)
     {
-		short maximum = -127;
+    	double maximum = -127.0;
 		
-		for(short current: list)
+		for(double current: list)
 		{
 			if(current>maximum)
 			{
@@ -130,11 +130,11 @@ public class RekursiverCyborg
     	return maximum;
     }
     
-    public short bestValueForBlack(ArrayList<Short> list)
+    public double bestValueForBlack(ArrayList<Double> list)
     {
-  		short minimum = 127;
+    	double minimum = 127.0;
   		
-  		for(short current: list)
+  		for(double current: list)
   		{
   			if(current<minimum)
   			{
