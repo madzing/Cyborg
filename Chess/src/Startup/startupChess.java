3970d238ffb32623d81b641525e2f04c58a1bd88
Jan <jan.metzing@gmx.net>
2021-08-12 15:52:11 +0200

    bla

--- Chess/src/Startup/startupChess.java
+++ Chess/src/Startup/startupChess.java
@@ -2,6 +2,7 @@ package Startup;
 
 import Fachwerte.Fen;
 import Material.Position;
+import Werkzeuge.RekursiverCyborg;
 
 public class startupChess
 {
@@ -9,7 +10,14 @@ public class startupChess
     public static void main(String[] args)
     {
 
+        RekursiverCyborg anton = new RekursiverCyborg(5);
         
+    	Fen startFen= Fen.select("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    	Position startPosition = new Position(startFen);
+    	
+        Position besteFolgePos = anton.getBestFollowingPosition(startPosition);
+        
+        System.out.println(besteFolgePos.getFen());
 
     }
 
