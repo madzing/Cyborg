cc28e19f6488630597d8082a3d503de4c164dcaf
Jan <jan.metzing@gmx.net>
2021-08-19 19:45:58 +0200

    kjsdah

--- Chess/src/Werkzeuge/AlphaBetaCyborgSpeedTest.java
+++ Chess/src/Werkzeuge/AlphaBetaCyborgSpeedTest.java
@@ -12,7 +12,7 @@ public class AlphaBetaCyborgSpeedTest {
 	@Test
 	// Fen für die Startposition als Ansicherung "r4rk1/1bpqbppp/p1np1n2/1p2p3/4P3/1BNP1N1P/PPPB1PP1/R2Q1RK1 w - - 3 11"
 	public void testSpeed() {
-		AlphaBetaCyborg anton = new AlphaBetaCyborg(5);
+		AlphaBetaCyborg anton = new AlphaBetaCyborg(6);
 
 		Fen startFen = Fen.select("r4rk1/1bpqbppp/p1np1n2/1p2p3/4P3/1BNP1N1P/PPPB1PP1/R2Q1RK1 w - - 3 11");
 		Position startPosition = new Position(startFen);
