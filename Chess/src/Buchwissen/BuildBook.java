540a51c47903410e248f2c7a55577add01ac95a6
Jan <jan.metzing@gmx.net>
2021-08-20 13:28:16 +0200

    fgs

--- Chess/src/Buchwissen/BuildBook.java
+++ Chess/src/Buchwissen/BuildBook.java
@@ -1,6 +1,10 @@
 package Buchwissen;
 
+import java.awt.EventQueue;
+import java.util.ArrayList;
+
 import Fachwerte.Fen;
+import Gui.ChessGui2;
 import Material.Position;
 import Services.PositionCalc;
 import Werkzeuge.AlphaBetaCyborg;
@@ -12,10 +16,17 @@ public class BuildBook {
 	Position _position;
 	Fen _fen;
 	
+	public static void main(String[] args) {
+		BuildBook bla = new BuildBook();
+	}
+	
 	public BuildBook() {
-		_fen = Fen.select("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+		// "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"				StartPosition
+		// "rnbqkbnr/pppppppp/8/8/2PPP3/1PN2NP1/PB1Q1PBP/3RR1K1 w kq - 0 1"             sollte gut für weiß sein
+
+		_fen = Fen.select("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1");
 		_position = new Position(_fen);
-		_cyborg = new AlphaBetaCyborg(6);
+		_cyborg = new AlphaBetaCyborg(7);
 		_positionCalc = new PositionCalc(_position);
 		
 		writeBook();
@@ -23,7 +34,16 @@ public class BuildBook {
 	
 	public void writeBook()
 	{
+		_cyborg.getBestFollowingPosition(_position);
+		ArrayList<Position> positions = _positionCalc.getLegalFollowingPositions();
 		
+		for (Position position : positions)
+		{
+			if(_cyborg.getGuteZuege().containsKey(position.getPlacement()))
+			{
+				System.out.println(position.getFen()+" /// "+_cyborg.getGuteZuege().get(position.getPlacement()));
+			}
+		}
 	}
 
 }
