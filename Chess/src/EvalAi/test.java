ed6746017bde0ee19d5945e5b65aa3cdc1be10b9
Jan <jan.metzing@gmx.net>
2021-08-26 16:19:23 +0200

    sdfgdf

--- Chess/src/EvalAi/test.java
+++ Chess/src/EvalAi/test.java
@@ -1,5 +1,17 @@
 package EvalAi;
 
+import org.tensorflow.*;
+import org.tensorflow.op.Ops;
+
 public class test {
 
+	
+public test()
+{
+	try(Graph graph = new Graph()) {
+	    Ops tf = Ops.create(graph);
+	    
+	    // Keras code here.
+	}
+}
 }
